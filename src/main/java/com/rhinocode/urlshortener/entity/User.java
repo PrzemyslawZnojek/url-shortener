25047f1b841bd29fde2f4a2116b365705dee557b
PrzemyslawZnojek <przemek.znojek@gmail.com>
2023-11-11 22:35:25 +0100

    Adjust User class

--- src/main/java/com/rhinocode/urlshortener/entity/User.java
+++ src/main/java/com/rhinocode/urlshortener/entity/User.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -14,17 +17,21 @@ public class User {
     @Id
     @GeneratedValue
     private UUID id;
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Invalid Email form", regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}",
+            flags = Pattern.Flag.CASE_INSENSITIVE)
     private String email;
     private String hashedPassword;
     private Instant insertedAt;
     private Instant updatedAt;
 
-    protected User() {}
+    User() {}
 
-    public User(String email, String hashedPassword, Instant insertedAt, Instant updatedAt) {
+    public User(String email, String hashedPassword) {
         this.email = email;
         this.hashedPassword = hashedPassword;
-        this.insertedAt = insertedAt;
-        this.updatedAt = updatedAt;
+        this.insertedAt = Instant.now();
+        this.updatedAt = Instant.now();
     }
 }
